f7898451777d2cecf414ec254e3074fcfde69e22
ajaiswal <aakanksha_jaiswal@mcafee.com>
2021-12-27 14:24:49 +0530

    add test for create method

--- stores/employee/employee.go
+++ stores/employee/employee.go
@@ -8,26 +8,9 @@ import (
 	"sample-api/stores"
 )
 
-/*
-	As a practice, we group imports from same domain. This makes the imports look clean.
-	All the files should follow the same pattern for imports ordering. Each group should be in alphabetical order.
-
-	Order for grouping imports:
-	1. Standard libraries
-	2. Groups of external libraries
-	3. Local imports
-*/
-
 type store struct {
 	db *sql.DB
 }
-
-/*
-	Interface Factory:
-	Factory functions can return interfaces instead of structs.
-	Interfaces allow you to define behavior without exposing internal implementation.
-	This means we can make structs private, while only exposing the interface outside our package.
-*/
 
 // New is the factory function for stores that injects database dependency.
 func New(db *sql.DB) stores.Employee {
--- stores/employee/employee_test.go
+++ stores/employee/employee_test.go
@@ -1,55 +1,67 @@
 package employee
 
-import "testing"
+import (
+	"database/sql"
+	"testing"
 
-/*
-	Test Driven Development (or Test First Development)  is a software  development approach where we write the tests
-	before writing the code to have a clear and unbiased understanding of what the code SHOULD do.
+	"sample-api/models"
+	"sample-api/stores"
+)
 
-	Writing the code first and test later leads to development of a biased opinion on what the code should do;
-	hence, we must write the tests first to avoid multiple iterations on the code.
-*/
+func initializeTests() stores.Employee {
+	var db *sql.DB
 
-/*
-	- Any test file in go must end with `_test.go`. This is how go recognises that the file ignored
-	while building the package.
-
-	- Each test function must be prefixed by `Test` and the signature for testing functions must be:
-	func TestXxx(*testing.T){
-		// test your code here
-	}
-
-	- Test function naming conventions:
-	To test a function named `Xyx` or `xyz`, the test name should be `TestXyx` (notice the Mixed case).
-	For a more specific unit test, we can add suffix to differentiate between the tests, like:
-	`TestXyzInvalidParameter`
-	`TestXyzSuccess`
-	`TestXyzError`
-	While running the tests for entire project, it is preferable to differentiate between the tests from
-	different packages:
-	`TestEmployee_XyzSuccess` // student package
-	(notice that the package name is followed by an `_`)
-
-	- Any new helper function or a custom type created for testing purpose should be added to the test file ONLY
-	to avoid being included in the package build.
-
-	- The source code file and its corresponding test file(s) must be in the same package.
-
-	--------------------------------------------------------------------------------------------------------------------
-	NOTE:
-	We can keep test files in a different package, but that approach is used for Black-box Testing,
-	which allows us to test only the exported functions/methods of a package, like for the `fmt` package
-	in go (not needed for now).
-
-	The white box approach, where the test and the production code are in the same package,
-	allows us to test both the non-exported and exported identifiers of the package.
-	This is the preferable approach when writing unit tests that require access to non-exported
-	variables, functions, and methods.
-
-*/
+	return New(db)
+}
 
 func TestEmployee_Create(t *testing.T) {
+	s := initializeTests()
 
+	tests := []struct {
+		desc  string
+		input models.Employee
+		id    int64 // output
+	}{
+		{"create with missing optional fields", models.Employee{Name: "Joey", Role: models.NonFaculty}, 1},
+		{"create with all fields", models.Employee{Name: "Jane", Designation: "Admin", Role: models.NonFaculty}, 2},
+		{"employee ID ignored for create request", models.Employee{ID: 10, Name: "Alex"}, 3},
+	}
+
+	for i, tc := range tests {
+		id, err := s.Create(tc.input)
+
+		if err != nil {
+			t.Errorf("TEST[%d], failed: %s\nExpected: nil, Got: %v", i, tc.desc, err)
+		}
+
+		if id != tc.id {
+			t.Errorf("TEST[%d], failed: %s\nExpected: %v, Got: %v", i, tc.desc, tc.id, id)
+		}
+	}
+}
+
+func TestEmployee_CreateError(t *testing.T) {
+	s := initializeTests()
+
+	tests := []struct {
+		desc  string
+		input models.Employee
+	}{
+		{"missing mandatory fields", models.Employee{Designation: "Admin"}},
+		{"database error", models.Employee{Name: "Chandler", Designation: "Professor"}},
+	}
+
+	for i, tc := range tests {
+		id, err := s.Create(tc.input)
+
+		if err == nil {
+			t.Errorf("TEST[%d], failed: %s\nExpected: error, Got: nil", i, tc.desc)
+		}
+
+		if id != 0 {
+			t.Errorf("TEST[%d], failed: %s\nExpected: no ID to be created, Got: %v", i, tc.desc, id)
+		}
+	}
 }
 
 func TestEmployee_GetAll(t *testing.T) {
